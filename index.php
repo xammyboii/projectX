544c56dd48e085faed4d607cb71d185ce8e169f1
xammyboii <118329638+xammyboii@users.noreply.github.com>
2022-11-21 08:53:09 -0600

    Create index.php
    Create home page of the CMS website.

---
+++ index.php
@@ -0,0 +1,23 @@
+<?php 
+/*
+ * Name: Xaviery Abados
+ * WEBD-2008 CMS Project
+ * Description: Home page of the CMS website.
+ */
+
+    require('connect.php');
+
+
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>X-Men CMS</title>
+</head>
+<body>
+    
+</body>
+</html>
