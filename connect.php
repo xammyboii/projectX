b8a6c942ced301c705d515a2d3e7c19e97449000
xammyboii <118329638+xammyboii@users.noreply.github.com>
2022-11-21 08:35:24 -0600

    Create connect.php
    Create script to connect to the database.

---
+++ connect.php
@@ -0,0 +1,21 @@
+<?php 
+/*
+ * Name: Xaviery Abados
+ * WEBD-2008 CMS Project
+ * Description: Script to connect to the database.
+ */
+
+    // Database Connection
+    define('DB_DSN','mysql:host=localhost;dbname=serverside;charset=utf8');
+    define('DB_USER','serveruser');
+    define('DB_PASS','gorgonzola7!');
+
+    try{
+    	$db = new PDO(DB_DSN, DB_USER, DB_PASS);
+    } catch(PDOException $e){
+    	print "Error: " . $e->getMessage();
+    	die(); // Forced execution
+    }
+
+    session_start();
+?>
