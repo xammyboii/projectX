75d958b75db9e7f3cb85c68e4fbd1ffcc8a02b30
xammyboii <118329638+xammyboii@users.noreply.github.com>
2022-12-13 23:20:35 -0600

    Update new_mutant.php
    Add image upload functionality when adding new mutant/content.

--- new_mutant.php
+++ new_mutant.php
@@ -6,39 +6,99 @@
  *      their choice in the website.
  */
     require('connect.php');
+    require('\xampp\htdocs\a\php-image-resize-master\lib\ImageResize.php');
+    require('\xampp\htdocs\a\php-image-resize-master\lib\ImageResizeException.php');
 
     $errorFlag = false;
-    if ($_POST && isset($_POST['new_mutant'])){
-        if (empty($_POST['x_alias'])){
-            $errorMsg = "Character name is required.";
-            $errorFlag = true;
-        }
-        else {
-            // Take the user_id from the logged in user
-            $user_id = $_SESSION['user_id'];
 
-            // Sanitize edited fields
-            $x_alias = filter_input(INPUT_POST, 'x_alias', FILTER_SANITIZE_FULL_SPECIAL_CHARS);
-            $x_name  = filter_input(INPUT_POST, 'x_name', FILTER_SANITIZE_FULL_SPECIAL_CHARS);
-            $x_power = filter_input(INPUT_POST, 'x_power', FILTER_SANITIZE_FULL_SPECIAL_CHARS);
-            $x_desc  = filter_input(INPUT_POST, 'x_desc', FILTER_SANITIZE_FULL_SPECIAL_CHARS);
+    // ******************************************************************************** Image Upload Functions
+    function file_upload_path($original_filename, $upload_subfolder_name = 'upload'){
+        $current_folder = dirname(__FILE__);
+        $path_segments = [$current_folder, $upload_subfolder_name, basename($original_filename)];
+        
+        return join(DIRECTORY_SEPARATOR, $path_segments);
+     }
 
-            // Insert query
-            $qry = "INSERT INTO xmen (x_name, x_alias, x_desc, x_power, user_id) VALUES (:x_name, :x_alias, :x_desc, :x_power, :user_id)";
-            $stmt = $db->prepare($qry);
-            
-            // Bind values & execute query
-            $stmt->bindValue(':x_alias', $x_alias);        
-            $stmt->bindValue(':x_name', $x_name);
-            $stmt->bindValue(':x_power', $x_power);
-            $stmt->bindValue(':x_desc', $x_desc);
-            $stmt->bindValue(':user_id', $user_id);
-            $stmt->execute();
-
-            header("Location: index.php");
-        }
+     function file_is_an_image($temporary_path, $new_path) {
+        $allowed_mime_types      = ['image/gif', 'image/jpeg', 'image/png'];
+        $allowed_file_extensions = ['gif', 'jpg', 'jpeg', 'png'];
+        
+        $actual_file_extension   = pathinfo($new_path, PATHINFO_EXTENSION);
+        $actual_mime_type        = getimagesize($temporary_path)['mime'];
+        
+        $file_extension_is_valid = in_array($actual_file_extension, $allowed_file_extensions);
+        $mime_type_is_valid      = in_array($actual_mime_type, $allowed_mime_types);
+        
+        return $file_extension_is_valid && $mime_type_is_valid;
     }
 
+    $file_image_detected = isset($_FILES['file_image']) && ($_FILES['file_image']['error'] === 0);
+    $file_error_detected = isset($_FILES['file_image']) && ($_FILES['file_image']['error'] > 0);
+
+    // ******************************************************************* Creating/Adding New X-Men Character
+    if ($_POST && isset($_POST['new_mutant'])){
+        if (strlen($_POST['x_alias']) > 0){
+            $user_id = $_SESSION['user_id']; // User Id of current Admin
+
+            // Sanitize Fields
+            $x_name  = filter_input(INPUT_POST, 'x_name', FILTER_SANITIZE_FULL_SPECIAL_CHARS);
+            $x_alias = filter_input(INPUT_POST, 'x_alias', FILTER_SANITIZE_FULL_SPECIAL_CHARS);
+            $x_desc  = filter_input(INPUT_POST, 'x_desc', FILTER_SANITIZE_FULL_SPECIAL_CHARS);
+            $x_power = filter_input(INPUT_POST, 'x_power', FILTER_SANITIZE_FULL_SPECIAL_CHARS);
+            filter_input(INPUT_POST, 'file_image',FILTER_SANITIZE_FULL_SPECIAL_CHARS);
+
+            // WITH File Image Uploaded
+            if ($file_image_detected){
+                $image_filename 	= $_FILES['file_image']['name'];
+                $temp_image_path 	= $_FILES['file_image']['tmp_name'];
+                $new_image_path 	= file_upload_path($image_filename);
+
+                if (file_is_an_image($temp_image_path, $new_image_path)){
+                    $img = new \Gumlet\ImageResize($temp_image_path);
+                    $img->resizeToWidth(400);
+                    $img->save($temp_image_path);
+
+                    move_uploaded_file($temp_image_path, $new_image_path);
+                    $x_image = $_FILES['file_image']['name'];
+
+                    $qry = "INSERT INTO xmen (x_name, x_alias, x_desc, x_power, x_image, user_id) VALUES (:x_name, :x_alias, :x_desc, :x_power, :x_image, :user_id)";
+                    $stmt = $db->prepare($qry);
+                    
+                    // Bind values & execute query
+                    $stmt->bindValue(':x_name', $x_name);
+                    $stmt->bindValue(':x_alias', $x_alias);        
+                    $stmt->bindValue(':x_desc', $x_desc);
+                    $stmt->bindValue(':x_power', $x_power);
+                    $stmt->bindValue(':x_image', $x_image);
+                    $stmt->bindValue(':user_id', $user_id);
+                    
+                    header("Location: index.php");
+                    exit();
+                }
+            }
+            else { // WITHOUT File Image Uploaded
+                $qry = "INSERT INTO xmen (x_name, x_alias, x_desc, x_power, user_id) VALUES (:x_name, :x_alias, :x_desc, :x_power, :user_id)";
+                $stmt = $db->prepare($qry);
+                $stmt->bindValue(':x_name', $x_name);
+                $stmt->bindValue(':x_alias', $x_alias);        
+                $stmt->bindValue(':x_desc', $x_desc);
+                $stmt->bindValue(':x_power', $x_power);
+                $stmt->bindValue(':user_id', $user_id);
+                
+                header("Location: index.php");
+                exit();
+            }
+            
+        } elseif (empty($_POST['x_alias'])){
+            $errorFlag = true;
+            $errorMsg = "Character name is required.";
+
+        } elseif ($file_error_detected){
+            $errorFlag = true;
+            $errorMsg = $_FILES['file_image']['error'];
+    
+        }
+    }
 ?>
 <!DOCTYPE html>
 <html lang="en">
@@ -46,13 +106,14 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" type="text/css" href="styles.css">
     <title>X-Men CMS: New</title>
 </head>
 <body>
 <?php require('header.php') ?>
 
     <main>
-        <form method="post" action="new_mutant.php" id="new_mutant_form">
+        <form method="POST" enctype='multipart/form-data' action="new_mutant.php" id="new_mutant_form">
             <fieldset>
 <?php if($errorFlag == true): ?>
                 <div class="error-box"><?= $errorMsg ?></div>
@@ -75,12 +136,15 @@
                     <textarea name="x_desc" id="text_desc"></textarea>
                 </p>
                 <p>
+                    <label for="x_image">Image Upload (optional)</label><br>
+                    <input type="file" name="file_image" id="image">
+                </p>
+                <p>
                     <input type="submit" name="new_mutant" id="add_new_mutant" value="Add New Mutant">
                 </p>
             </fieldset>
         </form>
-    </main>
-
 <?php require('footer.php') ?>
+    </main>
 </body>
 </html>
